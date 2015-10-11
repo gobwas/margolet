11c7f6a7b0d79865bfd30da738a7d9d2f2151d8b
gobwas <gobwas@gmail.com>
2015-10-11 20:27:39 +0300

    Fix

--- matcher.go
+++ matcher.go
@@ -1,7 +1,36 @@
 package telegram
 
+import "regexp"
+
 type Match map[string]interface{}
 
 type Matcher interface {
 	Match(text string) (*Match, bool)
 }
+
+type Equal struct {
+	pattern string
+}
+
+func (self Equal) Match(text string) (match *Match, ok bool) {
+	if self.pattern == text {
+		match = &Match{"text": text}
+		ok = true
+	}
+
+	return
+}
+
+// todo
+//type RegExp struct {
+//	pattern regexp.Regexp
+//}
+//
+//func (self RegExp) Match(text string) (match *Match, ok bool) {
+//	if self.pattern.Match([]byte(text)) {
+//		match = &Match{"text": text}
+//		ok = true
+//	}
+//
+//	return
+//}
